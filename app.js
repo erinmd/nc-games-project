0955286780ddfcb9ff7ab37898d794a882f4baf9
erinmd <erin817@gmail.com>
2023-02-20 12:19:28 +0000

    started getrview

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { handle500Error } = require('./controllers/error-handlers')
 const app = express()
 
 app.get('/api/categories', getCategories)
+// app.get('/api/reviews/:review_id', getReview)
 
 app.use(handle500Error)
 
