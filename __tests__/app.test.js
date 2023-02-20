7d3471d71ad76ff1fdef0551e62d4677ce23fe3a
erinmd <erin817@gmail.com>
2023-02-20 12:34:09 +0000

    add getReview endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -22,7 +22,6 @@ describe('api', () => {
       .get('/api/categories')
       .expect(200)
       .then(({ body: { categories } }) => {
-        expect(categories).toBeInstanceOf(Array)
         expect(categories).toHaveLength(4)
         categories.forEach(category => {
           expect(category).toMatchObject({
@@ -37,7 +36,6 @@ describe('api', () => {
       .get('/api/reviews')
       .expect(200)
       .then(({body:{reviews}}) => {
-        expect(reviews).toBeInstanceOf(Array)
         expect(reviews).toHaveLength(13)
         reviews.forEach(review => expect(review).toMatchObject({
           owner: expect.any(String),
@@ -52,6 +50,41 @@ describe('api', () => {
         })
       )
       expect(reviews).toBeSortedBy('created_at', {descending: true} )
+      })
+  })
+  test('200: GET request responds with a review object', () => {
+    return request(app)
+      .get('/api/reviews/2')
+      .expect(200)
+      .then(({body: {review}}) => {
+        expect(review).toEqual({
+          review_id: 2,
+          title: 'Jenga',
+          designer: 'Leslie Scott',
+          owner: 'philippaclaire9',
+          review_img_url:
+            'https://images.pexels.com/photos/4473494/pexels-photo-4473494.jpeg?w=700&h=700',
+          review_body: 'Fiddly fun for all the family',
+          category: 'dexterity',
+          created_at: "2021-01-18T10:01:41.251Z",
+          votes: 5
+        })
+      })
+  })
+  test('404: GET request with an id out of range returns "Not Found"', () => {
+    return request(app)
+      .get('/api/reviews/10000')
+      .expect(404)
+      .then(({body:{msg}}) => {
+        expect(msg).toBe('Review not found')
+      })
+  })
+  test('400: GET request with invalid id returns bad request', () => {
+    return request(app)
+      .get('/api/reviews/bananas')
+      .expect(400)
+      .then(({body:{msg}}) => {
+        expect(msg).toBe('Bad request')
       })
   })
 })
--- controllers/reviewsControllers.js
+++ controllers/reviewsControllers.js
@@ -1,7 +1,14 @@
-const {selectReviews} = require('../models/reviewsModel')
+const {selectReviews, selectReview} = require('../models/reviewsModel')
 
 exports.getReviews = (req, res, next) => {
     return selectReviews()
         .then(reviews => res.status(200).send({reviews}))
+        .catch(err => next(err))
+}
+
+exports.getReview = (req, res, next) => {
+    const {review_id} = req.params
+    return selectReview(review_id)
+        .then(review => res.status(200).send({review}))
         .catch(err => next(err))
 }
